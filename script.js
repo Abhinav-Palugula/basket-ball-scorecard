9d1d554ef46701ad8fc0c927e2d504c61f0d02fa
Abhinav Palugula <99081921+AbhinavPalugula@users.noreply.github.com>
2022-11-08 16:45:44 +0530

    added date feature

--- script.js
+++ script.js
@@ -72,3 +72,48 @@ function reset(){
   document.getElementById("guest-score").innerHTML = guestScore;
   document.getElementById("leading").innerHTML = "Leading: NONE"
 }
+function displaydate(){
+  const d = new Date();
+  var month = "";
+  switch (new Date().getMonth()) {
+    case 0:
+      month = "Jan";
+      break;
+    case 1:
+      month = "Feb";
+      break;
+    case 2:
+      month = "Mar";
+      break;
+    case 3:
+      month = "Apr";
+      break;
+    case 4:
+      month = "May";
+      break;
+    case 5:
+      month = "June";
+      break;
+    case 6:
+      month = "July";
+      break;
+    case 7:
+      month = "Aug";
+      break;
+    case 8:
+      month = "Sep";
+      break;
+    case 9:
+      month = "Oct";
+      break;
+    case 10:
+      month = "Nov";
+      break;
+    case 11:
+      month = "Dec";
+      break;
+  }
+  
+  document.getElementById("date").innerHTML = month + " " + d.getDate() + " " + d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes() + ":" +d.getSeconds();
+}
+setInterval(displaydate, 1000);
